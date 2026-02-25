5997497898dc09fcc4cb877a550f0d6e696e5f0b
Krishna07012008 <25113cn091@glbitm.ac.in>
2026-02-25 11:11:08 +0530

    Add files via upload
    calculator

---
+++ krishnanarayan09.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#include<math.h>
+int main()
+{
+    int a,b,c,x.d ;
+    printf("Enter value of a,b,c:");
+    scanf("%d%d%d",&a,&b,&c);
+    d=sqrt ((b*b)-(4*a*c));
+    id (d<=0){
+        printf("Two real and distinct roots")
+        printf(Enter 
+ 
+}
